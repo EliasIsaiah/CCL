d200a1dc5208dd6a0f4c4c5f876539b7e4c72c06
Elias Roussos <eliasisaiah@outlook.com>
2023-06-15 19:14:01 -0400

    implemented unordered list function

--- week5/word-processor/after/script.js
+++ week5/word-processor/after/script.js
@@ -20,3 +20,23 @@ function makeAParagraph(string) {
 function makeAnHtmlElementWithText(startingTag,text,endingTag) {
     return startingTag+text+endingTag;
 }
+
+function createThreeItemList(listItemsArray) {
+    return `<ul><li>${listItemsArray[0]}</li><li>${listItemsArray[1]}</li><li>${listItemsArray[2]}</li></ul>`
+}
+
+function createItemList(listItems) {
+    listMarkupString = "";
+    for (item of listItems) {
+        let listItem = `<li>${item}</li>`;
+        listMarkupString += listItem;
+    }
+    return "<ul>" + listMarkupString + "</ul>";  
+}
+
+// <ul>
+//     <li>my list item 1</li>
+//     <li>my list item 2</li>
+//     <li>my list item 3</li>
+//     <li>my list item 4</li>
+// </ul>
