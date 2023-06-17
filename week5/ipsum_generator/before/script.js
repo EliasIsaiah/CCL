8e608bb050d7dfdc2059c85c3832f58aac202c66
Elias Roussos <51466260+EliasIsaiah@users.noreply.github.com>
2023-06-16 20:52:50 -0400

    "before" assets created

---
+++ week5/ipsum_generator/before/script.js
@@ -0,0 +1,16 @@
+/* variables
+* number of paragraphs : number
+* paragraph size : number
+* punctuation : array[chars]
+* sentences : array[strings]
+* words : array[string]
+*/
+
+// function definitions
+/**
+* @param {number} numberOfParagraphs
+* @param {number} paragraphSize
+*/
+function generate(numberOfParagraphs, paragraphSize) {
+    console.log(numberOfParagraphs, paragraphSize);
+}
