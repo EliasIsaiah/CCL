d3552dc49a026194d0168ae00dd945ed52803c27
Elias Roussos <eliasisaiah@outlook.com>
2023-08-06 14:57:54 -0400

    stuff

--- week11/node/dot-env-demo/server.js
+++ week11/node/dot-env-demo/server.js
@@ -4,5 +4,4 @@ const SECRET_KEY = process.env.SECRET_KEY;
 const S3_BUCKET = process.env.S3_BUCKET;
 
 console.log(`SECRET_KEY retrieved: ${SECRET_KEY}`);
-// fetch("<your api endpoint>{apiKey}")
 console.log(`S3_BUCKET retrieved: ${S3_BUCKET}`);
