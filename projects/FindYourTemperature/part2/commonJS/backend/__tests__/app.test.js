7a78f11bf3d3aa4a75135fce8098f081e7e4efab
Elias Roussos <eliasisaiah@outlook.com>
2023-08-06 14:58:18 -0400

    modifications to project part II solution

--- projects/FindYourTemperature/part2/commonJS/backend/__tests__/app.test.js
+++ projects/FindYourTemperature/part2/commonJS/backend/__tests__/app.test.js
@@ -1,42 +1,54 @@
 const app = require("../app.js");
 const request = require("supertest");
 
-const MOCK_API_RESPONSE = { name: "dublin", main: { temp: 273.15 } };
+const MOCK_API_RESPONSE =
+{
+    name: "dublin",
+    main: {
+        temp: 273.15
+    }
+};
 
 global.fetch = jest.fn(() => Promise.resolve({
     json: () => Promise.resolve(MOCK_API_RESPONSE)
 }));
 
-test("GET /", async () => {
-    await request(app)
-        .get("/")
-        .expect(200)
-        .then(response => {
-            expect(response.text).toEqual("Hello from backend to frontend!");
-        })
+describe("when I hit the / endpoint of my api", () => {
+    it("responds with a message", async () => {
+        await request(app)
+            .get("/")
+            .expect(200)
+            .then(response => {
+                expect(response.text).toEqual("Hello from backend to frontend!");
+            })
+    })
 })
 
-test("POST /weather", async () => {
-    const body = { cityName: "" };
-
-    await request(app)
-        .post("/weather")
-        .send(body)
-        .expect(400)
-        .then(response => {
-            expect(response.body.weatherText).toEqual("City is not found!")
+describe("POST /weather", () => {
+    describe("when an empty string is sent for cityName", () => {
+        it("should respond with a 400 statusCode and an error message", async () => {
+            const body = { cityName: "" };
+            await request(app)
+                .post("/weather")
+                .send(body)
+                .expect(400)
+                .then(response => {
+                    expect(response.body.weatherText).toEqual("City is not found!")
+                })
         })
-})
+    })
+    describe("when a cityName is given", () => {
+        it("should retrieve weather data", async () => {
+            const body = { cityName: "dublin" };
+            const expected = "City Name: dublin, Temperature: 32 F";
 
-test("POST /weather with cityName", async () => {
-    const body = { cityName: "dublin" };
-    const expected = "City Name: dublin, Temperature: 32 F";
-
-    await request(app)
-        .post("/weather")
-        .send(body)
-        .expect(200)
-        .then(response => {
-            expect(response.body.weatherText).toEqual(expected)
-        });
+            await request(app)
+                .post("/weather")
+                .send(body)
+                .expect(200)
+                .then(response => {
+                    expect(response.body.weatherText).toEqual(expected)
+                });
+        })
+    })
 })
