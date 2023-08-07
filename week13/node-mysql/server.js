fc5b60d681f337c0bc714494615549095770b3d4
Elias Roussos <eliasisaiah@outlook.com>
2023-08-07 01:50:26 -0400

    removed comments with secrets

--- week13/node-mysql/server.js
+++ week13/node-mysql/server.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: process.env.DB_USER, // root
-    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD, 
     database: 'test_schema'
 })
 
