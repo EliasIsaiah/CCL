bc45a8ed47b0af7fb37c9f2ef8f7c7be666256f7
Elias Roussos <eliasisaiah@outlook.com>
2023-08-07 01:49:50 -0400

    mysql example

---
+++ week13/node-mysql/server.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv");
+const mysql = require("mysql");
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER, // root
+    password: process.env.DB_PASSWORD,
+    database: 'test_schema'
+})
+
+connection.connect();
+
+connection.query('SELECT * from person', (error, results, fields) => {
+    if (error) throw error; 
+    for (result of results) console.log(`<------------- Person id ${result.id}: ----------------->\nName: ${result.name}\nFavorite Color: ${result.favorite_color}\nFavorite Food: ${result.favorite_food}`);
+});
+
+connection.end();
