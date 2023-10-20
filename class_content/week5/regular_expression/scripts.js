4915b3b222e181e526121ee644c6dd34d1e9e013
Elias Roussos <eliasisaiah@outlook.com>
2023-10-20 17:19:12 -0400

    removed reg ex

--- class_content/week5/regular_expression/scripts.js
+++
@@ -1,2 +0,0 @@
-// replace every letter "a" with the letter "b" if a is the second character in the word
-const myString = "Woah! what is that in there? your mom. Ruby continued rolling around the thing in the box. it made a surprising noise that I couldn't identify";
