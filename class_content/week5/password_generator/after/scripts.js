5ca309366611f39b60f51e063faf3fb0c2db7baa
Elias Roussos <eliasisaiah@outlook.com>
2024-04-24 21:51:29 -0400

    improved validation test gives better feedback

--- class_content/week5/password_generator/after/scripts.js
+++ class_content/week5/password_generator/after/scripts.js
@@ -1,70 +1,92 @@
 /**
  * Given parameters length, caseChoice, and includeSpecialChars, generates a password to fit the specifications
- * 
- * @param {number} length 
- * @param {string} caseChoice 
- * @param {boolean} includeSpecialChars 
+ *
+ * @param {number} length
+ * @param {string} caseChoice
+ * @param {boolean} includeSpecialChars
  * @returns {string} generated password
  */
 
 function generatePassword(length, caseChoice, includeSpecialChars) {
+  if (!length || !caseChoice || typeof includeSpecialChars !== "boolean")
+    return "invalid input parameters";
 
-    if(!length || !caseChoice || typeof (includeSpecialChars) !== "boolean" ) return "invalid input parameters";
+  let desiredPassordLength = length;
+  let generatedPassword = [];
+  let doSpecialCharacters = includeSpecialChars;
 
-    let desiredPassordLength = length;
-    let generatedPassword = [];
-    let doSpecialCharacters = includeSpecialChars;
+  let doSpecialCharacterInThisLoop = false;
 
-    let doSpecialCharacterInThisLoop = false;
+  for (i = 0; i < desiredPassordLength; i++) {
+    if (doSpecialCharacters) doSpecialCharacterInThisLoop = Math.random() > 0.5;
 
-    for (i = 0; i < desiredPassordLength; i++) {
+    if (doSpecialCharacterInThisLoop) {
+      let randomSpecialCharCode = randomIntFromInterval(33, 64);
+      generatedPassword.push(String.fromCharCode(randomSpecialCharCode));
+    } else {
+      let randomChar;
+      let randomCharUpperCase = String.fromCharCode(
+        randomIntFromInterval(65, 90)
+      );
+      let randomCharLowerCase = String.fromCharCode(
+        randomIntFromInterval(97, 122)
+      );
 
-        if (doSpecialCharacters) doSpecialCharacterInThisLoop = Math.random() > .5
-
-        if (doSpecialCharacterInThisLoop) {
-            let randomSpecialCharCode = randomIntFromInterval(33, 64);
-            generatedPassword.push(String.fromCharCode(randomSpecialCharCode));
-
+      if (caseChoice === "random") {
+        if (Math.random() > 0.5) {
+          randomChar = randomCharLowerCase;
         } else {
-            let randomChar;
-            let randomCharUpperCase = String.fromCharCode(randomIntFromInterval(65, 90));
-            let randomCharLowerCase = String.fromCharCode(randomIntFromInterval(97, 122));
-
-            if (caseChoice === "random") {
-                if (Math.random() > .5) {
-                    randomChar = randomCharLowerCase;
-                } else {
-                    randomChar = randomCharUpperCase;
-                }
-            }
-            else if (caseChoice === "upper") {
-                randomChar = randomCharUpperCase;
-            } else if (caseChoice === "lower") {
-                randomChar = randomCharLowerCase;
-            } 
-
-            generatedPassword.push(randomChar);
+          randomChar = randomCharUpperCase;
         }
+      } else if (caseChoice === "upper") {
+        randomChar = randomCharUpperCase;
+      } else if (caseChoice === "lower") {
+        randomChar = randomCharLowerCase;
+      }
+
+      generatedPassword.push(randomChar);
     }
-    return generatedPassword.join("");
+  }
+  return generatedPassword.join("");
 }
 
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function testPassword(password) {
+  //   const re =
+  //     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[$&+,:;=?@#|'<>.^*()%!-]).{5,}$/;
+  const containsDigits = /(?=.*\d)/.test(password);
+  const containsLowerCase = /(?=.*[a-z])/.test(password);
+  const containsUpperCase = /(?=.*[A-Z])/.test(password);
+  const containsSpecialCharacter = /(?=.*[$&+,:;=?@#|'<>.^*()%!-])/.test(
+    password
+  );
+  const isAtLeastFiveChars = /^.{5,}$/.test(password);
+  return {
+    password,
+    containsDigits,
+    containsLowerCase,
+    containsUpperCase,
+    containsSpecialCharacter,
+    isAtLeastFiveChars,
+  };
 }
 
 // testing
 
-// let password0 = generatePassword(50, "random", true);
-// let password1 = generatePassword(50, "random", false);
-// let password2 = generatePassword(50, "lower", true);
-// let password3 = generatePassword(50, "lower", false);
-// let password4 = generatePassword(50, "upper", true);
-// let password5 = generatePassword(50, "upper", false);
+let password0Result = testPassword(generatePassword(50, "random", true));
+let password1Result = testPassword(generatePassword(50, "random", false));
+let password2Result = testPassword(generatePassword(50, "lower", true));
+let password3Result = testPassword(generatePassword(50, "lower", false));
+let password4Result = testPassword(generatePassword(50, "upper", true));
+let password5Result = testPassword(generatePassword(50, "upper", false));
 
-// console.log(`password with random case and special characters: \n${password0}`);
-// console.log(`password with random case and no special characters: \n${password1}`);
-// console.log(`password with all lowercase and special characters: \n${password2}`);
-// console.log(`password with all lowercase and no special characters: \n${password3}`);
-// console.log(`password with all uppercase and special characters: \n${password4}`);
-// console.log(`password with all uppercase and no special characters: \n${password5}`);
+console.log(password0Result);
+console.log(password1Result);
+console.log(password2Result);
+console.log(password3Result);
+console.log(password4Result);
+console.log(password5Result);
